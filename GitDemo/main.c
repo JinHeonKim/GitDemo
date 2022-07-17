3bbe728b7ede2eca2387467a9018b593e916be8a
JinHeonKim <kimjh102412@naver.com>
2022-07-17 16:37:26 +0900

    Initial Commit

---
+++ GitDemo/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  GitDemo
+//
+//  Created by 김진헌 on 2022/07/17.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
